69bb351288b7a9d876f4b1eb26a841b40f31c5a8
Angelo Polo <polo.language@gmail.com>
2014-06-01 23:00:50 -0400

    Nightly

--- js/go.js
+++ js/go.js
@@ -1,14 +1,23 @@
+// global consts:
+var whiteStoneImg;
+var blackStoneImg;
+
+// __main__:
 function go() {
   'use strict';
-  buildBoard();
+  var theBoard;
 
+  buildBoard(theBoard, 9);
+  //TEST__placeStones(theBoard);
+
+  // Templates:
   function Board(boardSize) {
     var col, row;
     this.imgSrc;
     this.grid = new Array(boardSize);
-    for (col = 0; col < 9; ++col) {
-      this.grid[col] = new Array(9);
-      for (row = 0; row < 9; ++row) {
+    for (col = 0; col < boardSize; ++col) {
+      this.grid[col] = new Array(boardSize);
+      for (row = 0; row < boardSize; ++row) {
         this.grid[col][row] = new Intersection();
       }
     }
@@ -17,54 +26,63 @@ function go() {
   function Intersection() {
     this.left;
     this.top;
-    this.content;
+    this.stone;
     this.image;
   }
 
-  function buildBoard() {
-    var boardSize = 9;
-    var boardDiv;
-    var theBoard = new Board(boardSize);
+  // Methods:
+  function buildBoard(theBoard, boardSize) {
     var topMost, leftMost, stoneDiameter,
         intersection,
         col, row;
-    try {
-      switch (boardSize) {
-        case 9:
-          theBoard.imgSrc = 'images/board_9x9_550x550.png';
-          topMost = 5;
-          leftMost = 5;
-          stoneDiameter = 60
-          break;
-        case 13:
-          theBoard.imgSrc = 'images/board_13x13_550x550.png';
-          break;
-        case 19:
-          theBoard.imgSrc = 'images/board_19x19_550x550.png';
-          break;
-        default:
-          throw 'Invalid boardSize passed to makeBoard()';        
-      }
-      // set intersection coordinates
-      for (col = 0; col < 9; ++col) {
-        for (row = 0; row < 9; ++row) {
-          intersection = theBoard.grid[col][row];
-          intersection.top = topMost + stoneDiameter * row;
-          intersection.left = leftMost + stoneDiameter * col;
-        }
-      }
-      // set board background image
-      boardDiv = document.getElementById('board_img');
-      boardDiv.src = theBoard.imgSrc;
-    } catch(e) {
-      console.error('Unable to load board!');
-      console.log(e);
+    //var boardHtml = '';
+    var boardDiv = document.getElementById('board');
+    switch (boardSize) {
+      case 9:
+        theBoard = new Board(boardSize);
+        theBoard.imgSrc = 'images/board_9x9_550x550.png';
+        whiteStoneImg = 'images/stone_white_60x60.png';
+        blackStoneImg = 'images/stone_black_60x60.png';
+        topMost = 5;
+        leftMost = 5;
+        stoneDiameter = 60
+        //boardDiv.style.padding = "5px";
+        break;
+      case 13:
+        theBoard = new Board(boardSize);
+        theBoard.imgSrc = 'images/board_13x13_550x550.png';
+        break;
+      case 19:
+        theBoard = new Board(boardSize);
+        theBoard.imgSrc = 'images/board_19x19_550x550.png';
+        break;
+      default:
+        throw 'Invalid boardSize passed to makeBoard()';        
     }
+    // set intersection coordinates
+    for (col = 0; col < 9; ++col) {
+      for (row = 0; row < 9; ++row) {
+        intersection = theBoard.grid[col][row];
+        intersection.top = topMost + stoneDiameter * row;
+        intersection.left = leftMost + stoneDiameter * col;
+        //boardHtml += '<div class="cell"></div>';
+      }
+    }
+    // set board background image
+    boardDiv.style.backgroundImage = 'url("' + theBoard.imgSrc + '")';
   }
-}
 
+  function placeStone(theBoard, col, row, color) {
+    var intersection = theBoard.grid[col][row];
+      if (color !== 'w' && color !== 'b')
+        throw 'Invalid color passed to placeStone()';
+      intersection.stone = color;    
+  }
 
+  function TEST__placeStones(theBoard) {
+    var intersections = [[0,0, 'w'], [4,2, 'w'], [8,3, 'b']];
 
-
+  }
+} // END: go()
 
 
